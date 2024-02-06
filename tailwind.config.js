92ce8128cd5f962fe59cd4aa85733d7669ee539c
admin <moladeijarotimi@gmail.com>
2024-02-06 17:26:53 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const { wedgesTW } = require("@lemonsqueezy/wedges");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "node_modules/@lemonsqueezy/wedges/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [wedgesTW()],
+};
