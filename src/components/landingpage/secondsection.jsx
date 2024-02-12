257b089307423850806d07d723e2889e510bc413
admin <moladeijarotimi@gmail.com>
2024-02-12 15:04:48 +0100

    second section landing page

---
+++ src/components/landingpage/secondsection.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import TitleBanner from "../common/titlebanner";
+import Card from "../common/card";
+import SecdSectionImg from "../../assets/secondsection.png";
+import Card1 from "../../assets/1.png";
+import Card2 from "../../assets/2.png";
+import Card3 from "../../assets/3.png";
+import Card4 from "../../assets/4.png";
+import BannerImg from "../../assets/bolt.png";
+
+const SecondSection = () => {
+  return (
+    <div className="flex gap-12 max-w-[1304px] mx-auto px-6 py-12 mt-24 text-white items-center rounded-[48px] bg-[#FFFFFF0D] -z-10">
+      <div className="flex-1">
+        <img src={SecdSectionImg} className="max-w-[604px] h-auto" />
+      </div>
+      <div className="flex flex-col gap-2 flex-1">
+        <TitleBanner>
+          How it Works
+          <img src={BannerImg} className="h-8 w-8" />
+        </TitleBanner>
+        <div className="flex flex-wrap gap-4">
+          <Card
+            icon={Card1}
+            title="Sign up"
+            content="Join the Creatipp community by creating your profile. It's quick, easy, and absolutely free."
+          ></Card>
+          <Card
+            icon={Card2}
+            title="Setup your campaign goal"
+            content="Define your creative project or cause, set a funding goal, and launch your campaign. Tell your story and inspire your audience."
+          ></Card>
+          <Card
+            icon={Card3}
+            title="Receive crypto donations"
+            content="Watch as your supporters contribute in various cryptocurrencies. Creatipp handles the complexities, ensuring a smooth donation process."
+          ></Card>
+          <Card
+            icon={Card4}
+            title="Withdraw funds"
+            content="When your campaign is successful, withdraw your funds in your"
+          ></Card>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SecondSection;
--- src/pages/landingpage.jsx
+++ src/pages/landingpage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import HeroSection from "../components/landingpage/herosection";
 import FirstSection from "../components/landingpage/firstsection";
+import SecondSection from "../components/landingpage/secondsection";
 
 const LandingPage = () => {
   return (
     <div className="bg-black">
       <HeroSection />
       <FirstSection />
+      <SecondSection />
     </div>
   );
 };
